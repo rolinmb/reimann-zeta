f7f1a6d4b4f08c611821da8d2552d605b7f97bb7
Rolin Blake <rolinmblake@gmail.com>
2024-02-25 19:49:03 -0600

    something is happening im not sure what, doesnt seem right tho

--- src/main.py
+++ src/main.py
@@ -1,2 +1,35 @@
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import numpy as np
+import mpmath
+# extended / analytically continued Reimann Zeta Function
+"""def reimann_zeta(s, precision, sumterms=100):
+    mpmath.mp.dps = precision
+    return mpmath.nsum(lambda n: 1 / (n ** s), [1, mpmath.inf], method="euler-mclaurin", terms=sumterms)
+"""
+def mp_reimann_zeta(s, precision):
+    mpmath.mp.dps = precision
+    return mpmath.zeta(s)
+
 if __name__ == "__main__":
-    print("hello world")
+    """xvals = [0.1 * i for i in range(1, 100)]
+    yvals = [reimann_zeta(complex(0.5, x), 30).real for x in xvals]
+    plt.plot(xvals, yvals, label="Reimann Zeta Function")
+    plt.title("Reimann Zeta Function")
+    plt.xlabel("Real Part(s)")
+    plt.ylabel("Real Value")
+    plt.legend()
+    plt.show()
+    """
+    real_vals = np.linspace(-5, 5, 100)
+    imag_vals = np.linspace(-5, 5, 100)
+    real_part, imag_part = np.meshgrid(real_vals, imag_vals)
+    zeta_values = np.vectorize(lambda x, y: mp_reimann_zeta(complex(x, y), 30))(real_part, imag_part)
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.plot_surface(real_part, imag_part, np.abs(zeta_values), cmap='viridis', alpha=0.8)
+    ax.set_title('Analytically Continued Riemann Zeta Function')
+    ax.set_xlabel('Real Part (s)')
+    ax.set_ylabel('Imaginary Part (s)')
+    ax.set_zlabel('Real Value')
+    plt.show()
